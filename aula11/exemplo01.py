d3142c282e712c9a146ef9e94fb3e37deb25fafd
PEDRO HENRIQUE DE OLIVEIRA RAIMUNDO <pedro.raimundo@aluno.senac.rio>
2026-01-06 19:34:24 -0300

    lista N

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -8,7 +8,13 @@ print(n,m)
 # -------------------------------------------------
 # Gerar numeros decimais
 
-n_dicinal = random.uniform(1,10)
-numero_decimal = round(n_dicinal,1) #Arredondar 
-print(f'{n_dicinal:2f}') # formatar 
-print(numero_decimal)
+# n_dicinal = random.uniform(1,10)
+# numero_decimal = round(n_dicinal,1) #Arredondar 
+# print(f'{n_dicinal:2f}') # formatar 
+# print(numero_decimal)
+
+# --------------------------------------------------
+#Gerar  numeros aleatorios dentro da lista 
+
+lst_numeros = [random.randint(1, 10) for num in range(5)]
+print(lst_numeros)
---
+++ aula11/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+lst_numeros = [random.randint(1, 10) for num in range(5)]
+print(lst_numeros)
