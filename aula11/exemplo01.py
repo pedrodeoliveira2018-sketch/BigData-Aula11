38d05e6e7e09de5f08c6f85074b9a15ecb049414
PEDRO HENRIQUE DE OLIVEIRA RAIMUNDO <pedro.raimundo@aluno.senac.rio>
2026-01-06 19:17:35 -0300

    Import DCN

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -2,4 +2,13 @@
 import random
 
 n = random.randint(1, 10)
-print(n)
+m = random.randint(1, 10)
+print(n,m)
+
+# -------------------------------------------------
+# Gerar numeros decimais
+
+n_dicinal = random.uniform(1,10)
+numero_decimal = round(n_dicinal,1) #Arredondar 
+print(f'{n_dicinal:2f}') # formatar 
+print(numero_decimal)
